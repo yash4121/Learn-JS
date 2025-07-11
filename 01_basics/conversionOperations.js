64f4afa1393e06e2a3ae6dc84a7b3f63d15b3ca6
Yash Yeltiwar <boostbrain2021@gmail.com>
2025-07-11 16:44:10 +0000

    conversions

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,19 @@
+// let num = 23
+// console.log(typeof num)
+// let num = "23"
+// num = Number(num)
+// console.log(typeof num)
+// let num = null
+// num = Number(num)
+// console.log(num)
+// console.log(typeof num)
+let num = "33abs"
+num = Number(num)
+console.log(num)
+console.log(typeof num)
+//Number class converts into number but for null it converts it into NaN , for not valid string Nan
+//true ->1 
+let isValid = 1
+isValid = Boolean(isValid)
+console.log(isValid)
+//1 -> true, 0 -> false, "" -> false, "aa" -> true
