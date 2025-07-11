d8eb8a1f01cbb771f701788e6d5d26cbaa437be1
Yash Yeltiwar <boostbrain2021@gmail.com>
2025-07-11 16:07:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as new version
+// alert("Hello") // only works in browser or html
+let name = "Ram" //string
+console.log(typeof (name))
+let age = 23 //int 2^53, for big numbers we have bigInt
+console.log(typeof(age))
+let isValid = true //boolean
+console.log(typeof(isValid))
+let temperature = null //null is standalone value special type in js object.
+console.log(typeof(temperature))
+
+//undefined is I have declared the variable but not yet initialized yet. 
+//null is I have empty value in the variable.
