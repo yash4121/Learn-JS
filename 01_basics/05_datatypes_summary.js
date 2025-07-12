c8a6a06ecc50cd844988e053dfa03fccef42529d
Yash Yeltiwar <boostbrain2021@gmail.com>
2025-07-12 14:37:11 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,26 @@
+// on the basis of how data is stored and accessed it is divided into primitive and non-primitive datatypes
+//Primitives
+
+// number, String, Boolean, null, undefined, Symbol, BigInt
+
+const Id = Symbol('123')
+const Id2 = Symbol('123') //Symbol is used to store unique id
+console.table([Id, Id2]) //not equal
+
+const bigNumber = 21234535653646767632443n
+
+// Non Primitives
+
+// Array, Object, Functions
+
+const names = ["Ram", "Sham"]
+console.log(names[0]) //Ram
+const obj = {
+    "name": "Ram",
+    "age": 23
+}
+console.log(obj["name"]) //Ram
+const myfunc = function() {
+    console.log("hi there")
+}
+myfunc() //hi there
