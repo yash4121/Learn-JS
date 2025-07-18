9fbdae579114cbaf53570cd1ff70bc26ca2ef4c6
Yash Yeltiwar <boostbrain2021@gmail.com>
2025-07-18 18:09:15 +0000

    arrays-1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+const myArr = [1,2,3,"school",false,0]
+console.log(myArr[0])
+const collegTypes = ["IIT", "NIT", "IIIT"]
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2)
+// myArr.push(6) 6 at the end
+// console.log(myArr)
+// myArr.pop() pop 6 from end
+// console.log(myArr)
+// myArr.unshift(9) push 9 at index 0
+// console.log(myArr)
+// myArr.shift() pop from index 0
+// console.log(myArr)
+// console.log(myArr.includes(1)) // true
+// console.log(myArr.indexOf(4)) //-1
+// const newArr = myArr.join()
+// console.log(newArr)
+console.log("A ", myArr)
+const myn1 = myArr.slice(1,4)
+console.log(myn1)
+console.log("B ", myArr)
+const myn2 = myArr.splice(1,4)
+console.log(myn2)
+console.log("C ", myArr)
