5c4201d905998f0db51ff508270d6d7ac3495fd0
Yash Yeltiwar <boostbrain2021@gmail.com>
2025-08-02 20:15:44 +0000

    for loop

---
+++ 05_iterators/one.js
@@ -0,0 +1,19 @@
+//  for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element)
+    
+//  }
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`i is ${i}`);
+//         console.log(`j is ${j}`);
+//     }
+    
+// }
+const myArray = [1,2,3]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element)
+    
+}
+// break and continue
